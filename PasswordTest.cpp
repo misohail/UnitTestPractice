c956da03e81a167a462e33d8879c82232b249a60
Muhammad Sohail <misohail@csuchico.edu>
2025-11-05 11:41:18 -0800

    Add unit tests

--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -20,3 +20,31 @@ TEST(PasswordTest, single_letter_password)
 	int actual = my_password.count_leading_characters("Z");
 	ASSERT_EQ(1, actual);
 }
+
+TEST(PasswordTest, empty_string)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("");
+	ASSERT_EQ(0, actual);
+}
+
+TEST(PasswordTest, no_repeat)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("abc");
+	ASSERT_EQ(1, actual);
+}
+
+TEST(PasswordTest, repeats_at_start)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("aaaaXYZ");
+	ASSERT_EQ(4, actual);
+}
+
+TEST(PasswordTest, case_sensitive_repeat)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("aaA");
+	ASSERT_EQ(1, actual);
+}
